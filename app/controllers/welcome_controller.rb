cc420034069ec5a76ea662a4b8a3c9c71c891161
catm705 <catgelinas78@gmail.com>
2014-05-09 16:52:00 -0400

    Used Bootstrap CSS/JS for welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,16 @@ class WelcomeController < ApplicationController
 
   end
 
+
+
+  def search
+
+    # The BYOB.ar way
+    # @doc = Nokogiri::XML(@response)
+    # @contact_emails = @doc.xpath("//gd:email").each {|x| puts x.attribute("address")}
+
+    # Josh Guthals Way
+    # text: Nokogiri
+  end
+
 end
