4e1d216ca19892479cf99cc34dbeb538912ec530
catm705 <catgelinas78@gmail.com>
2014-05-14 12:16:45 -0400

    removed column from user model - no longer needed b/c we're not going to have them be able to send emails from their account

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140512165507) do
+ActiveRecord::Schema.define(version: 20140514161450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
