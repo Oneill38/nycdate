18e6545dcfc9dc2b683413bbc0e0e5257b9cc511
catm705 <catgelinas78@gmail.com>
2014-05-15 14:04:52 -0400

    js file

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('#general_event_search form').on('submit', function(event){
+    alert("Did this work?");
+    event.preventDefault();
+    var event_search_date = $('option').val();
+
+    $.ajax({
+      url: "/searches", //Routes to searches controller - not about page!
+      type: 'POST',
+      dataType: 'json',
+      data: {date_input: event_search_date}
+      }).done(function(data){
+        $.each(data, function(index, value){
+          var event_div = $('<div>').html(value);
+          $('#general_event_results').append(event_div);
+        });
+      });
+
+  });
+});
+
+
